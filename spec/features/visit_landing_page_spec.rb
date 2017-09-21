ca5445bff9aef85347be8b29cc3fe162da2ce888
Toby Privett <toby@snaplab.co.uk>
2017-09-21 13:13:53 +0100

    fix up feature

--- spec/features/visit_landing_page_spec.rb
+++ spec/features/visit_landing_page_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-feature "visit the landing page" do
-  scenario "in general" do
-    visit "/"
+feature 'visit the landing page' do
+  scenario 'in general' do
+    visit '/'
 
-    expect(page).to have_css("h1", "DIT Digital Workspace")
+    expect(page).to have_css('h1', 'DIT Digital Workspace')
   end
 end
