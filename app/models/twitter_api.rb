d06d92ec0c13b1bcf934ba728b6246afa3650cbf
Dan Beckett <dan.beckett@door4.com>
2017-09-29 18:09:49 +0100

    rubocopped twitter_api.rb

--- app/models/twitter_api.rb
+++ app/models/twitter_api.rb
@@ -2,15 +2,15 @@ require 'twitter'
 
 class TwitterApi
   def self.public_tweets
-  	# we only display 3 tweets, but "count" is taken before filtering out replies and rts, so overegged this
-  	#client.user_timeline('DannnBeckett', count: 3)
-  	client.user_timeline('GOVUK', count: 100, exclude_replies: true, include_rts: false)
+    # we only display 3 tweets, but "count" is taken before filtering out replies and rts, so overegged this
+    # client.user_timeline('DannnBeckett', count: 3)
+    client.user_timeline('GOVUK', count: 100, exclude_replies: true, include_rts: false)
   end
 
   def self.client
-    @twitterClient ||= Twitter::REST::Client.new do |config|
-      config.consumer_key    = "2Wrbb5ZHkDrsuTqadZhjZd53W"
-      config.consumer_secret = "qr7qUZLixqg6cfKx9u86AqLlExtNJfXt35SlwljII9RTXWgFZ1"
+    @twitter_client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key    = '2Wrbb5ZHkDrsuTqadZhjZd53W'
+      config.consumer_secret = 'qr7qUZLixqg6cfKx9u86AqLlExtNJfXt35SlwljII9RTXWgFZ1'
     end
   end
 end
