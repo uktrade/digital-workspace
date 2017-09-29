6eee8ba03613259d8337a4b89cf4bdceb6945c56
Toby Privett <toby@snaplab.co.uk>
2017-09-29 10:06:13 +0100

    Update home page test assertion

--- spec/features/visit_landing_page_spec.rb
+++ spec/features/visit_landing_page_spec.rb
@@ -4,7 +4,7 @@ describe 'visit the landing page', vcr: true do
   scenario 'in general' do
     visit '/'
 
-    expect(page).to have_link('Peoplefinder')
+    expect(page).to have_link('DIT Digital Workspace')
     expect(page).to have_css('h2', text: 'What\'s popular?')
   end
 end
