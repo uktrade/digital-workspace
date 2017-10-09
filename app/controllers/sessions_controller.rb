6b7cfe041683ea6a5f79c6d66dae0f0ece4b25f1
Toby Privett <toby@snaplab.co.uk>
2017-10-09 17:06:23 +0100

    Ensure session user access the User class

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :ensure_sso_user, only: [:create]
 
   def create
-    session[:auth_user] = AuthUser.new(request.env['omniauth.auth'])
+    session[:auth_user] = User.from_omniauth_hash(request.env['omniauth.auth'])
     redirect_to '/'
   end
 end
