7574475181e10357ab8066877025c2e210edb1e2
Peter Broom <me@peterbroom.co.uk>
2017-10-10 18:03:44 +0100

    truncate and read more link helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def parsed_tweet(tweet)
     TweetParser.new(tweet).parse.html_safe
   end
+
+  def truncate_excerpt(text)
+    html_text = truncate(text, :length => 200, :omission => "..." , :escape => false)
+    strip_tags(html_text)
+  end
+
+  def read_more(link)
+      link_to("Read more", link)
+  end
 end
