baebf9361ecc9daabe55074b204dd3fd3c87ab69
Peter Broom <me@peterbroom.co.uk>
2017-10-11 11:03:44 +0100

    category selector functionality

--- app/assets/javascripts/category-select.js
+++ app/assets/javascripts/category-select.js
@@ -1,1 +1,9 @@
-console.log('select category');
+// On document load
+document.addEventListener('DOMContentLoaded',function() {
+	document.getElementById("select_category").onchange=category_page;
+}, false);
+
+function category_page(event) {
+	if( event.target.value )
+		location.assign(event.target.value);
+}
