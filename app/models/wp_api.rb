fb0d5fe13faa7cd8a12d664afb7547dcfc4f6ef4
Toby Privett <toby@snaplab.co.uk>
2017-10-12 09:25:35 +0100

    Introduce WpApi.get_json_headers

--- app/models/wp_api.rb
+++ app/models/wp_api.rb
@@ -4,9 +4,15 @@ class WpApi
 
   class << self
     def get_json_body(path)
-      uri = URI.join(BASE_URI, path).to_s
-      response = HTTParty.get(uri)
-      JSON.parse(response.body)
+      JSON.parse(get_json(path).body)
+    end
+
+    def get_json_headers(path)
+      JSON.parse(get_json(path).headers)
+    end
+
+    def get_json(path)
+      HTTParty.get(URI.join(BASE_URI, path).to_s)
     end
   end
 end
