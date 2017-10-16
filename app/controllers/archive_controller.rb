be524794511ef072905e2c1779a76eb7b7464b6a
Toby Privett <toby@snaplab.co.uk>
2017-10-16 15:19:25 +0100

    Tidy up views and partials

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -18,13 +18,23 @@ class ArchiveController < ApplicationController
 
   def init_defaults
     @page = (params[:page] || 1).to_i
-    @posts = @api_call.main_query(@page)
-
-    @hero_post = @posts.first
-    @posts = @posts[1..-1]
+    init_pagination
+    init_posts
 
     @categories = @api_call.other_categories_query
-    @posts_headers = @api_call.main_query_headers
     @comments_headers = @api_call.main_comments_headers
+  end
+
+  def init_pagination
+    headers = @api_call.main_query_headers(@page)
+    @paginator =
+      Paginator.new(@page, headers['x-wp-total'], headers['x-wp-totalpages'])
+  end
+
+  def init_posts
+    @posts = @api_call.main_query(@page)
+
+    @hero_post = @posts.first if @posts.first.is_a?(Hash)
+    @posts = @posts[1..-1] || []
   end
 end
