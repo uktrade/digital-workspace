15d58b5746b83f3885884a71f39c8541f38c30bd
Peter Broom <me@peterbroom.co.uk>
2017-10-16 09:01:14 +0100

    partial pagination and peoplefinder profile

--- app/models/news_type_queries.rb
+++ app/models/news_type_queries.rb
@@ -7,6 +7,10 @@ class NewsTypeQueries
     WpApi.get_json_body("pages?type=news&_embed&filter[news_category]=#{@slug}")
   end
 
+  def main_query_headers
+    WpApi.get_headers('pages?type=news&_embed')
+  end
+
   def other_categories_query
     WpApi.get_json_body('news_category')
   end
