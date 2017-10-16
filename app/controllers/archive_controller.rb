98d6ba153020a4ae9409126cf75423c82907fb69
Toby Privett <toby@snaplab.co.uk>
2017-10-16 13:54:04 +0100

    DRY up the archives controller

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -1,17 +1,23 @@
 class ArchiveController < ApplicationController
   def index
     @api_call = NewsQueries.new
-    @posts = @api_call.main_query
-    @categories = @api_call.other_categories_query
-    @posts_headers = @api_call.main_query_headers
-    @comments_headers = @api_call.main_comments_headers
+
+    init_defaults
+
     @id = params[:id]
   end
 
   def news_type
     @slug = params[:path].split('/').last
     @api_call = NewsTypeQueries.new(@slug)
-    @posts = @api_call.main_query
+
+    init_defaults
+  end
+
+  protected
+
+  def init_defaults
+    @posts = @api_call.main_query(params[:page] || 1)
     @categories = @api_call.other_categories_query
     @posts_headers = @api_call.main_query_headers
     @comments_headers = @api_call.main_comments_headers
