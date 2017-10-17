7db7d97ebc1d9d8534208fa90b6551ad01536db2
Peter Broom <me@peterbroom.co.uk>
2017-10-17 22:18:35 +0100

    News category title and breadcrumb

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -2,4 +2,12 @@ module NewsHelper
   def news_cats(name, slug)
     link_to(name, '/news/category/' + slug)
   end
+
+  def news_cat_for_title
+    WpApi.get_json_body("news_category/?slug=#{@slug}")
+  end
+
+  def news_cat_title
+    news_cat_for_title.first['name']
+  end
 end
