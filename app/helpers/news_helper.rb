b674843467eda80338084df2c50031dba2879f6b
Peter Broom <me@peterbroom.co.uk>
2017-10-20 11:37:12 +0100

    helper for date and timezone

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -10,4 +10,9 @@ module NewsHelper
   def news_cat_title
     news_cat_for_title.first['name']
   end
+
+  def get_date(date)
+    date = DateTime.parse(date)
+    date.strftime('%d %B %Y') + date.strftime(' @ %I:%M GMT')
+  end
 end
