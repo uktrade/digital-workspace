4dfb7f57a9a930757bf5fe38119fcf7beef11805
Peter Broom <me@peterbroom.co.uk>
2017-10-24 09:01:27 +0100

    add height animation and 'view more', 'view less'

--- app/assets/javascripts/topics.js
+++ app/assets/javascripts/topics.js
@@ -2,6 +2,8 @@
 
 	var toggle_cta = document.getElementsByClassName("topic-toggle");
 	var toggle_length = toggle_cta.length;
+	var flag = true;
+
 
 	function init_topics(){
 		if (typeof(toggle_cta) != 'undefined' && toggle_cta != null) {
@@ -18,8 +20,20 @@
 
 	// Toggle topics
 	function toggle_topics(){
-		var theClass = this.className;  // "this" is the element clicked
-		console.log( theClass );
+		var theClass = this.className;
+		var container = this.parentNode.parentNode.querySelector(':scope > .topics-list-extra');
+
+		this.parentNode.parentNode.classList.toggle("visible");
+
+		if (flag === true) {
+			container.style.height = container.scrollHeight + 'px';
+			this.innerHTML = 'View less';
+			flag = false;
+		} else {
+			container.style.height = '0px';
+			this.innerHTML = 'View more';
+			flag = true;
+		}
 	};
 
 	init_topics();
