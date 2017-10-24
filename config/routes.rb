2721174f30de0cdead254bc3340c3e74128e5073
Peter Broom <me@peterbroom.co.uk>
2017-10-24 12:48:39 +0100

    preview css and html

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@ Rails.application.routes.draw do
   match '/auth/:provider/callback', to: 'sessions#create', via: %i[get post]
 
   root to: 'home#index'
-  get '/news-and-views', to: 'archive#index'
-  get '/news-and-views/category/*path', to: 'archive#news_type'
-  get '/news-and-views/*path', to: 'single#news'
+  get '/news', to: 'archive#index'
+  get '/news/category/', to: 'archive#index'
+  get '/news/category/*path', to: 'archive#news_type'
+  get '/news/*path', to: 'single#news'
   get '/working-at-dit', to: 'topics#index'
-  get '/topic/*path', to: 'topics#index'
-  get '/howdoi/*path', to: 'single#index'
-  get '/policies-procedures/*path', to: 'single#index'
-  get '/form-templates/*path', to: 'single#index'
+  get '/topic/*path', to: 'content#content'
   get '/preview', to: 'preview#index'
   get '*path', to: 'single#index'
 end
