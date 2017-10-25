0411ec5d0795f162318f6ff1ba2b3c0798708250
Peter Broom <me@peterbroom.co.uk>
2017-10-25 11:09:04 +0100

    link helpers news, single and home pages

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,5 +1,17 @@
 module LinksHelper
-  def link_top_howdoi
+  def link_howdoi_top
     return '/howdoi/'
+  end
+
+  def link_policies_top
+    return '/policies-guidance/'
+  end
+
+  def link_forms_docs_top
+    return '/forms-templates/'
+  end
+
+  def link_news_top
+    return '/news/'
   end
 end
