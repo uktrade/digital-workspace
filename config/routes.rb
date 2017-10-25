05909b3ce675abc121ba8358c04f6c1f3efaf3b1
Peter Broom <me@peterbroom.co.uk>
2017-10-25 09:34:26 +0100

    topics route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/news/category/*path', to: 'archive#news_type'
   get '/news/*path', to: 'single#news'
   get '/working-at-dit', to: 'topics#index'
-  get '/topic/*path', to: 'content#content'
+  get '/topic/*path', to: 'topics#index'
   get '/preview', to: 'preview#index'
   get '*path', to: 'single#index'
 end
