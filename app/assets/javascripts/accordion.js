9d47b81f16aff9253b2600e2660a94992b1203e4
Peter Broom <me@peterbroom.co.uk>
2017-10-26 12:13:31 +0100

    open all functionality

--- app/assets/javascripts/accordion.js
+++ app/assets/javascripts/accordion.js
@@ -1,9 +1,11 @@
 (function() {
 
 	// Set some variables
-	var accordion_cta = document.getElementsByClassName("accordion-header");
+	var accordion_cta = document.getElementsByClassName('accordion-header');
+	var accordion_open_all_cta = document.getElementsByClassName('accordion-open-all');
 	var accordion_length = accordion_cta.length;
-	var flag = null;
+	var acc_single_flag = null;
+	var acc_all_flag = null;
 
 	// Initialise
 	function init_accordion(){
@@ -14,9 +16,13 @@
 
 	// Events
 	function accordion_events(){
-		for (i =0; i < accordion_length; i++) {
+		// Open single
+		for (var i =0; i < accordion_length; i++) {
 			accordion_cta[i].addEventListener('click', toggle_accordion, false);
 		}
+
+		// Open all
+		accordion_open_all_cta[0].addEventListener('click', toggle_all_accordion, false);
 	}
 
 	// Toggle topics
@@ -24,17 +30,39 @@
 		var self = this;
 		var container = self.parentNode.querySelector(':scope > .accordion-inner');
 
-		if (self.flag !== true) {	// open
-			self.parentNode.parentNode.classList.add('visible');
+		if (self.acc_single_flag !== true) {	// open
+			self.parentNode.classList.add('visible');
 			container.style.height = container.scrollHeight + 'px';
-			// self.innerHTML = 'View less';
-			self.flag = true;
-		} else {				// close
-			self.parentNode.parentNode.classList.remove('visible');
+			self.acc_single_flag = true;
+		} else {															// close
+			self.parentNode.classList.remove('visible');
 			container.style.height = '0px';
-			// self.innerHTML = 'View more';
-			self.flag = false;
+			self.acc_single_flag = false;
 		}
+	};
+
+	// Toggle all topics
+	function toggle_all_accordion(){
+		var self = this;
+		var accordions = document.querySelectorAll('.accordion-inner'), i;
+
+		if (self.acc_all_flag !== true) {		// open
+			self.acc_all_flag = true;
+		} else {														// close
+			self.acc_all_flag = false;
+		}
+
+		for (i = 0; i < accordions.length; ++i) {
+				if (self.acc_all_flag !== true) {		// open
+					accordions[i].parentNode.classList.add('visible');
+					accordions[i].style.height = accordions[i].scrollHeight + 'px';
+				} else {														// close
+					accordions[i].parentNode.classList.remove('visible');
+					accordions[i].style.height = '0px';
+				}
+		}
+
+		console.log('acc_all_flag after = ' + self.acc_all_flag);
 	};
 
 	init_accordion();
