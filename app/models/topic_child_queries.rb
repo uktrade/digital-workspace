9512297c9de88c54054e13fba0cfab01fd7a7132
Peter Broom <me@peterbroom.co.uk>
2017-10-26 12:14:26 +0100

    howdoi taxonomy

--- app/models/topic_child_queries.rb
+++ app/models/topic_child_queries.rb
@@ -1,10 +1,14 @@
 class TopicChildQueries
+  def initialize(slug)
+    @slug = slug
+  end
+
   def topic_howdoi_query
-    WpApi.get_json_body('howdoi?per_page=100&topic_taxonomy=24')
+    WpApi.get_json_body("howdoi?per_page=100&filter[topic_taxonomy]=#{@slug}")
   end
 
   def topic_policies_query
-    WpApi.get_json_body('policies')
+    WpApi.get_json_body("policies?per_page=100")
   end
 
   def topic_forms_query
