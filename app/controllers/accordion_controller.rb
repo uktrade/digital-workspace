13374136d9bc71dcf806d48b7ed4df8fd5e289ee
Peter Broom <me@peterbroom.co.uk>
2017-10-27 14:24:40 +0100

    initial api calls

--- app/controllers/accordion_controller.rb
+++ app/controllers/accordion_controller.rb
@@ -3,6 +3,7 @@ class AccordionController < ApplicationController
     @slug = params[:path].split('/').last
     @api_call = AccordionQueries.new(@slug)
     @howdois = @api_call.main_query(@slug)
-    @howdois_topic = @api_call.howdois_topic_query
+    @howdois_theme = @api_call.howdois_theme_query
+    @howdois_topics = @api_call.howdois_topic_query
   end
 end
--- app/models/accordion_queries.rb
+++ app/models/accordion_queries.rb
@@ -7,7 +7,11 @@ class AccordionQueries
     WpApi.get_json_body("howdoi?slug=#{@slug}")
   end
 
-  def howdois_topic_query
+  def howdois_theme_query
     WpApi.get_json_body("themes")
+  end
+
+  def howdois_topic_query
+    WpApi.get_json_body("topics?filter[theme_taxonomy]=0")
   end
 end
