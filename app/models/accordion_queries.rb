a7b941c616d70fc487ea8c796d340d06b2ea86e0
Peter Broom <me@peterbroom.co.uk>
2017-10-27 15:38:53 +0100

    hardcoded endpoints for testing

--- app/models/accordion_queries.rb
+++ app/models/accordion_queries.rb
@@ -8,10 +8,10 @@ class AccordionQueries
   end
 
   def howdois_theme_query
-    WpApi.get_json_body("themes")
+    WpApi.get_json_body("theme_taxonomy?filter[theme_taxonomy]=working-in-government")
   end
 
   def howdois_topic_query
-    WpApi.get_json_body("topics?filter[theme_taxonomy]=0")
+    WpApi.get_json_body("topics?theme_taxonomy=17")
   end
 end
