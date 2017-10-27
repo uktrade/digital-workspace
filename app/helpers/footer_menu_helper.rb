a1e40286d728a2774321e53d56f26624ff88716b
Peter Broom <me@peterbroom.co.uk>
2017-10-27 15:46:17 +0100

    fix for new footer nav

--- app/helpers/footer_menu_helper.rb
+++ app/helpers/footer_menu_helper.rb
@@ -1,61 +1,13 @@
 module FooterMenuHelper
-  def footer_menu_1
-    WpApi.get_json_body('menus?slug=networks')
+  def footer_menu
+    WpApi.get_json_body('menus?slug=footer-menu')
   end
 
-  def footer_menu_1_content
-    footer_menu_1.first['acf']['menu']
+  def footer_menu_content
+    footer_menu.first['acf']['menu']
   end
 
-  def footer_menu_1_title
-    footer_menu_1.first['title']['rendered']
-  end
-
-  def footer_menu_2
-    WpApi.get_json_body('menus?slug=teams')
-  end
-
-  def footer_menu_2_content
-    footer_menu_2.first['acf']['menu']
-  end
-
-  def footer_menu_2_title
-    footer_menu_2.first['title']['rendered']
-  end
-
-  def footer_menu_3
-    WpApi.get_json_body('menus?slug=sectors')
-  end
-
-  def footer_menu_3_content
-    footer_menu_3.first['acf']['menu']
-  end
-
-  def footer_menu_3_title
-    footer_menu_3.first['title']['rendered']
-  end
-
-  def footer_menu_4
-    WpApi.get_json_body('menus?slug=uk-regions')
-  end
-
-  def footer_menu_4_content
-    footer_menu_4.first['acf']['menu']
-  end
-
-  def footer_menu_4_title
-    footer_menu_4.first['title']['rendered']
-  end
-
-  def footer_menu_5
-    WpApi.get_json_body('menus?slug=international-regions')
-  end
-
-  def footer_menu_5_content
-    footer_menu_5.first['acf']['menu']
-  end
-
-  def footer_menu_5_title
-    footer_menu_5.first['title']['rendered']
+  def footer_menu_title
+    footer_menu.first['title']['rendered']
   end
 end
