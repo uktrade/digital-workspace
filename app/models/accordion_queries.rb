5bedc47d542c30be3140759aac919a3d724ac630
Peter Broom <me@peterbroom.co.uk>
2017-10-27 15:48:52 +0100

    rubocoped

--- app/models/accordion_queries.rb
+++ app/models/accordion_queries.rb
@@ -8,10 +8,10 @@ class AccordionQueries
   end
 
   def howdois_theme_query
-    WpApi.get_json_body("theme_taxonomy?filter[theme_taxonomy]=working-in-government")
+    WpApi.get_json_body('theme_taxonomy?filter[theme_taxonomy]=working-in-government')
   end
 
   def howdois_topic_query
-    WpApi.get_json_body("topics?theme_taxonomy=17")
+    WpApi.get_json_body('topics?theme_taxonomy=17')
   end
 end
