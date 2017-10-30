92aed9b8c5644a9ab5993af10439de4f8b496138
Peter Broom <me@peterbroom.co.uk>
2017-10-30 09:53:00 +0000

    endpoints

--- app/models/accordion_queries.rb
+++ app/models/accordion_queries.rb
@@ -4,14 +4,18 @@ class AccordionQueries
   end
 
   def main_query
-    WpApi.get_json_body("howdoi?slug=#{@slug}")
+    WpApi.get_json_body("howdoi/?slug=#{@slug}")
   end
 
-  def howdois_theme_query
-    WpApi.get_json_body('theme_taxonomy?filter[theme_taxonomy]=working-in-government')
+  def topics_query
+    WpApi.get_json_body("topics/")
   end
 
-  def howdois_topic_query
-    WpApi.get_json_body('topics?theme_taxonomy=17')
+  def howdois_theme_query(parent_slug)
+    WpApi.get_json_body("theme_taxonomy/?filter[theme_taxonomy]=#{@parent_slug}")
+  end
+
+  def howdois_topic_query(parent_topics_slug)
+    WpApi.get_json_body("topics/?filter[theme_taxonomy]=#{@parent_topics_slug}")
   end
 end
