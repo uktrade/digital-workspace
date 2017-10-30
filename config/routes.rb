7c32f59c0185ea42480ca44b183d6451738cb091
Peter Broom <me@peterbroom.co.uk>
2017-10-30 09:31:34 +0000

    standard and policy page template paths

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get '/news-and-views', to: 'archive#index'
   get '/news-and-views/category/*path', to: 'archive#news_type'
   get '/news-and-views/*path', to: 'single#news'
-  get '/working-at-dit', to: 'topics#index'
+  get '/working-at-dit', to: 'standard#index'
+  get '/working-at-dit/*path', to: 'topics#index'
   get '/topic/*path', to: 'topics#index'
   get '/howdoi/*path', to: 'accordion#index'
   get '/policies-procedures/*path', to: 'single#index'
@@ -13,5 +14,7 @@ Rails.application.routes.draw do
   get '/preview', to: 'preview#index'
   get '/accordion', to: 'accordion#index'
   get '/content/*path', to: 'content#content'
+  get '/policies-guidance/*path', to: 'policy#content'
+  get '/standard/*path', to: 'standard#index'
   get '*path', to: 'single#index'
 end
