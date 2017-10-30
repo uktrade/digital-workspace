36baf86c5f53e6e3d26c4a2b14a61ae8838a041a
Peter Broom <me@peterbroom.co.uk>
2017-10-30 14:10:22 +0000

    first level uri paths

--- app/controllers/accordion_controller.rb
+++ app/controllers/accordion_controller.rb
@@ -1,6 +1,10 @@
 class AccordionController < ApplicationController
+  def current_url_without_parameters
+    request.base_url + request.path
+  end
+
   def index
-    @slug = params[:path].split('/').last
+    @slug = current_url_without_parameters.split('/').last
     @api_call = AccordionQueries.new(@slug)
     @howdois = @api_call.main_query
     @howdois_theme = @api_call.howdois_theme_query
