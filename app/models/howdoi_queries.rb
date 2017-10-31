2fee98a6c2a43003a8a7616af2e87396b43d16e6
Peter Broom <me@peterbroom.co.uk>
2017-10-31 08:09:57 +0000

    how do I template controller and queries

---
+++ app/models/howdoi_queries.rb
@@ -0,0 +1,13 @@
+class HowdoiQueries
+  def initialize(slug)
+    @slug = slug
+  end
+
+  def howdoi_query
+    WpApi.get_json_body("howdoi?slug=#{@slug}")
+  end
+
+  def topics_related_news
+    WpApi.get_json_body("news?filter[topic_taxonomy]=#{@slug}&per_page=5")
+  end
+end
