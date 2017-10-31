ccb5c534f0544e05a1098418c9e97000298eba3f
Peter Broom <me@peterbroom.co.uk>
2017-10-31 08:10:31 +0000

    policy related news

--- app/controllers/policy_controller.rb
+++ app/controllers/policy_controller.rb
@@ -1,7 +1,19 @@
 class PolicyController < ApplicationController
+  def index
+    @api_call = PolicyQueries.new
+    init_posts
+  end
+
   def content
     @slug = params[:path].split('/').last
     @api_call = PolicyQueries.new(@slug)
     @content = @api_call.policy_query
+    @related_news = @api_call.topics_related_news
+  end
+
+  protected
+
+  def init_posts
+    @posts = @api_call.main_query
   end
 end
--- app/models/policy_queries.rb
+++ app/models/policy_queries.rb
@@ -6,4 +6,8 @@ class PolicyQueries
   def policy_query
     WpApi.get_json_body("policies?slug=#{@slug}")
   end
+
+  def topics_related_news
+    WpApi.get_json_body("news?filter[topic_taxonomy]=#{@slug}&per_page=5")
+  end
 end
