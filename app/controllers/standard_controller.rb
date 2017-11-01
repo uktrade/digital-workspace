1c3639276a45c212de03432dc70e1056ef1dff18
Peter Broom <me@peterbroom.co.uk>
2017-11-01 11:33:38 +0000

    target children by parent by id

--- app/controllers/standard_controller.rb
+++ app/controllers/standard_controller.rb
@@ -7,6 +7,7 @@ class StandardController < ApplicationController
     @slug = current_url_without_parameters.split('/').last
     @api_call = StandardQueries.new(@slug)
     @content = @api_call.main_query(@slug)
-    @content_children = @api_call.standard_child_query(@slug)
+    @parent_id = @content.first["id"]
+    @content_children = @api_call.standard_child_query(@parent_id)
   end
 end
