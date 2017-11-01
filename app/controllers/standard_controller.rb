dc4e9e2c41a27d91d12980b4359901369914cfd5
Peter Broom <me@peterbroom.co.uk>
2017-11-01 15:40:22 +0000

    raked

--- app/controllers/standard_controller.rb
+++ app/controllers/standard_controller.rb
@@ -7,7 +7,7 @@ class StandardController < ApplicationController
     @slug = current_url_without_parameters.split('/').last
     @api_call = StandardQueries.new(@slug)
     @content = @api_call.main_query(@slug)
-    @parent_id = @content.first["id"]
+    @parent_id = @content.first['id']
     @content_children = @api_call.standard_child_query(@parent_id)
   end
 end
