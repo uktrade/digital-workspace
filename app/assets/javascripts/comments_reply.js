644ba508507a4bbddfb250d71de5f7f52054cf7c
Dan Beckett <dan.beckett@door4.com>
2017-11-02 16:15:28 +0000

    widgetised comment display and got single-layer threading working when displaying

--- app/assets/javascripts/comments_reply.js
+++ app/assets/javascripts/comments_reply.js
@@ -1,11 +1,15 @@
+// Some actions for when replying to a comment - get ID of
+// comment to reply to, and focus user on the text box
+
 (function() {
 	var comment_links = document.getElementsByClassName('cta-comment-reply'),
 	    reply_input = document.getElementById('comment_parent');
 
 	for(var i = 0; i < comment_links.length; i++) {
 	     comment_links[i].addEventListener('click', function(){
-	     	var id = this.getAttribute("data-reply-to");
+	     	var id = this.getAttribute('data-reply-to');
 	     	reply_input.value = id;
+	     	document.getElementById('comment_content').focus();
 	     });
 	}
 
