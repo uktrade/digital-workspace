c5c63e4dbc92e295fde8f90c47ab14ed27a02d9e
Peter Broom <me@peterbroom.co.uk>
2017-11-02 12:59:41 +0000

    combine hash array and get count

--- app/models/standard_queries.rb
+++ app/models/standard_queries.rb
@@ -7,7 +7,15 @@ class StandardQueries
     WpApi.get_json_body("pages?type=standard_index&slug=#{@slug}")
   end
 
-  def standard_child_query(parent_id)
+  def standard_child_content_query(parent_id)
     WpApi.get_json_body("pages?type=content&orderby=title&order=asc&per_page=100&parent=#{parent_id}")
+  end
+
+  def standard_child_standard_query(parent_id)
+    WpApi.get_json_body("pages?type=standard_index&orderby=title&order=asc&per_page=100&parent=#{parent_id}")
+  end
+
+  def content_children(arr_a, arr_b)
+    arr_a + arr_b
   end
 end
