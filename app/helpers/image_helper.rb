1c9e90252d0c8fde07d3077ba3a15c079bbec5dd
Toby Privett <toby@snaplab.co.uk>
2017-11-02 09:08:22 +0000

    Check the image path is valid for wordpress images

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,17 +1,24 @@
 module ImageHelper
   def large_image_tag(post)
-    return unless post['acf'].present? && post['acf']['image']['sizes'].present?
+    return unless image_exists?(post)
     image_tag post['acf']['image']['sizes']['large'], alt: post['title']['rendered']
   end
 
   def medium_image_tag(post)
-    return unless post['acf'].present? && post['acf']['image']['sizes'].present?
+    return unless image_exists?(post)
     image_tag post['acf']['image']['sizes']['medium'], alt: post['title']['rendered']
   end
 
   def small_image_tag(post)
-    return unless post['acf'].present? && post['acf']['image']['sizes'].present?
-
+    return unless image_exists?(post)
     image_tag post['acf']['image']['sizes']['small'], alt: post['title']['rendered']
+  end
+
+  def image_exists?(obj)
+    # inspect the object's type
+    if obj.key?('acf')
+      # it is a word press image then we know how to check it
+      obj['acf'].present? && obj['acf']['image'] && obj['acf']['image']['sizes'].present?
+    end
   end
 end
