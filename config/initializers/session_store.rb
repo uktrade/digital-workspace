4e27de17639cd41a1021e1bafa19bd0774b92206
Toby Privett <toby@snaplab.co.uk>
2017-11-02 17:01:18 +0000

    Introduce redid-session-store
    - This client provides support for TLS

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,10 @@
 if ENV['REDIS_SESSION_URL'].present? && !Rails.env.test?
-  DigitalWorkspace::Application
-    .config
-    .session_store :redis_store, servers: [ENV['REDIS_SESSION_URL']]
+  DigitalWorkspace::Application.config.session_store :redis_session_store, {
+    key: 'your_session_key',
+    redis: {
+      expire_after: 120.minutes,
+      key_prefix: 'workspace:session:',
+      url: ENV['REDIS_SESSION_URL'],
+    }
+  }
 end
