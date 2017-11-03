407870c63613600a3d822a863f6542c86b4d14db
Dan Beckett <dan.beckett@door4.com>
2017-11-03 15:32:30 +0000

    news comments now post during page reload and appear on the next page. JS reworked to not use a data- attribute and just get value from ID (as old IE won't support data-). added some notification styling

--- app/controllers/single_controller.rb
+++ app/controllers/single_controller.rb
@@ -9,6 +9,12 @@ class SingleController < ApplicationController
     @slug = params[:path].split('/').last
     @api_call = PageQueries.new(@slug)
     @posts = @api_call.main_query
+
+    if params[:commit] == "Submit comment"
+      post_body = { "post" => params[:post], "author_email" => @people_finder_profile.email, "author_name" => @people_finder_profile.name, "content" => params[:comment_content], "parent" => params[:comment_parent] }
+      @response = WpApi.post_comment_json(post_body)
+    end
+
     @comments = @api_call.main_comments_query
     @comments_headers = @api_call.main_comment_headers
     @categories = @api_call.other_categories_query
