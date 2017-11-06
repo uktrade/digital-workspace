7ea62c60cd77f21e8d6b9ce75d4dcc19746f9817
Dan Beckett <dan.beckett@door4.com>
2017-11-06 11:18:26 +0000

    updated comments to use cache if there hasn't been one freshly posted

--- app/controllers/single_controller.rb
+++ app/controllers/single_controller.rb
@@ -9,8 +9,13 @@ class SingleController < ApplicationController
     @slug = params[:path].split('/').last
     @api_call = PageQueries.new(@slug)
     @posts = @api_call.main_query
-    comments_post if params[:commit] == 'Submit comment'
-    comments_print
+    if params[:commit] == 'Submit comment'
+      comments_post
+      comments_print
+    else
+      comments_print_cache
+    end
+    @categories = @api_call.other_categories_query
   end
 
   def comments_post
@@ -27,6 +32,10 @@ class SingleController < ApplicationController
   def comments_print
     @comments = @api_call.main_comments_query
     @comments_headers = @api_call.main_comment_headers
-    @categories = @api_call.other_categories_query
+  end
+
+  def comments_print_cache
+    @comments = @api_call.main_comments_query_cache
+    @comments_headers = @api_call.main_comment_headers_cache
   end
 end
