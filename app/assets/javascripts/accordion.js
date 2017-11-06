d2b86f0114830ff17d331394964cdebe16072b25
Peter Broom <me@peterbroom.co.uk>
2017-11-06 17:57:08 +0000

    refactor logic for accordion

--- app/assets/javascripts/accordion.js
+++ app/assets/javascripts/accordion.js
@@ -62,23 +62,34 @@
 	function toggle_single(){
 		var self = this;
 		var accordions = document.querySelectorAll('.accordion');
+		var accordions_length = accordions.length -1;
+		var visible = document.querySelectorAll('.visible');
 		var accordion = self.parentNode;
 		var accordions_inner = document.querySelectorAll('.accordion-inner');
 		var accordion_inner = self.parentNode.querySelector(':scope > .accordion-inner');
 
 		// Remove classes
 		for (i = 0; i < accordions.length; ++i) {
-			accordions[i].classList.remove('visible');
-			accordions_inner[i].style.height = '0px';
+
+			// Add class
+			if ( accordion.className === 'accordion visible' ) {
+				accordion.classList.remove('visible');
+				accordion_inner.style.height = '0px';
+			} else { // Remove class
+				accordion.classList.add('visible');
+				accordion_inner.style.height = accordion_inner.scrollHeight + 'px';
+			}
 
 			// Reset 'Open all'
 			flag = false;
 			accordion_all_cta[0].innerHTML = 'Open all';
-		}
-		// Add class
-		if ( accordion_inner.className === 'accordion-inner' ) {
-			accordion.classList.add('visible');
-			accordion_inner.style.height = accordion_inner.scrollHeight + 'px';
+
+
+			// Reset 'Close all'
+			if (accordions_length === visible.length) {
+				flag = true;
+				accordion_all_cta[0].innerHTML = 'Close all';
+			}
 		}
 	};
 
