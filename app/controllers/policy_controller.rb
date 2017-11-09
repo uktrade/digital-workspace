a2905908efded63fe12b0183d283580dd2913467
Peter Broom <me@peterbroom.co.uk>
2017-11-09 12:20:45 +0000

    global message bug fix

--- app/controllers/policy_controller.rb
+++ app/controllers/policy_controller.rb
@@ -9,7 +9,6 @@ class PolicyController < ApplicationController
     @content = @api_call.policy_query
     @related_news = @api_call.topics_related_news
 
-    @global_notifications = @api_call.main_query(@slug)
-    @global_notification = @global_notifications.first if @global_notifications.first.is_a?(Hash)
+    @global_notification = @content.first if @content.first.is_a?(Hash)
   end
 end
