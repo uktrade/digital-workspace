2033c5e4d51d9f9b22e0d0ba070e53d3a4ac9c12
Dan Beckett <dan.beckett@door4.com>
2017-11-12 22:03:14 +0000

    refactored to pass rubocop

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,12 +1,22 @@
 class ReportController < ApplicationController
+  require 'zendesk_api'
+  require 'json'
 
   def index
-    require 'zendesk_api'
-    require 'json'
+    client = zendesk_client
+    client.tickets.create(
+      subject: "Website error submission #{Time.now}",
+      comment: {
+        value: params['problem_report_problem']
+      },
+      submitter_id: client.current_user.id,
+      priority: 'normal', type: 'incident', custom_fields: zendesk_request_fields
+    )
+    redirect_back fallback_location: '/'
+  end
 
-    @params = params
-
-    client = ZendeskAPI::Client.new do |config|
+  def zendesk_client
+    ZendeskAPI::Client.new do |config|
       config.url = ENV['ZD_URL']
       config.username = ENV['ZD_USER']
       config.password = ENV['ZD_PASS']
@@ -14,8 +24,17 @@ class ReportController < ApplicationController
       require 'logger'
       config.logger = Logger.new(STDOUT)
     end
+  end
 
-    client.tickets.create(:subject => "Website error submission #{Time.now}", :comment => { :value => params['problem_report_problem']}, :submitter_id => client.current_user.id, :priority => "normal", :type => "incident", :custom_fields => [{:id => "114101595573", :value => params['problem_report_goal']},{:id => "114101595593", :value => params['problem_report_problem']},{:id => "114101595613", :value => params['problem_report_origin']},{:id => "114101582834", :value => params['problem_report_browser']}])
-    redirect_back fallback_location: '/'
+  def zendesk_request_fields
+    [{
+      id: '114101595573', value: params['problem_report_goal']
+    }, {
+      id: '114101595593', value: params['problem_report_problem']
+    }, {
+      id: '114101595613', value: params['problem_report_origin']
+    }, {
+      id: '114101582834', value: params['problem_report_browser']
+    }]
   end
 end
