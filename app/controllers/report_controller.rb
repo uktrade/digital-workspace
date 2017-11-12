267f3ff2bc63140bc5dff25b9ac70b6182b48c65
Dan Beckett <dan.beckett@door4.com>
2017-11-12 22:03:14 +0000

    refactored to pass rubocop

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,5 +1,4 @@
 class ReportController < ApplicationController
-<<<<<<< HEAD
   require 'zendesk_api'
   require 'json'
 
@@ -18,16 +17,6 @@ class ReportController < ApplicationController
 
   def zendesk_client
     ZendeskAPI::Client.new do |config|
-=======
-
-  def index
-    require 'zendesk_api'
-    require 'json'
-
-    @params = params
-
-    client = ZendeskAPI::Client.new do |config|
->>>>>>> got this successfully posting to test account, but needs correct information and styling
       config.url = ENV['ZD_URL']
       config.username = ENV['ZD_USER']
       config.password = ENV['ZD_PASS']
@@ -35,7 +24,6 @@ class ReportController < ApplicationController
       require 'logger'
       config.logger = Logger.new(STDOUT)
     end
-<<<<<<< HEAD
   end
 
   def zendesk_request_fields
@@ -48,10 +36,5 @@ class ReportController < ApplicationController
     }, {
       id: '114101582834', value: params['problem_report_browser']
     }]
-=======
-
-    client.tickets.create(:subject => "Website error submission #{Time.now}", :comment => { :value => params['problem_report_problem']}, :submitter_id => client.current_user.id, :priority => "normal", :type => "incident", :custom_fields => [{:id => "114101595573", :value => params['problem_report_goal']},{:id => "114101595593", :value => params['problem_report_problem']},{:id => "114101595613", :value => params['problem_report_origin']},{:id => "114101582834", :value => params['problem_report_browser']}])
-    redirect_back fallback_location: '/'
->>>>>>> got this successfully posting to test account, but needs correct information and styling
   end
 end
