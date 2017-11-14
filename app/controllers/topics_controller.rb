89e6d14de1e613b1e6eb8f06b7bebcf330da44ff
Peter Broom <me@peterbroom.co.uk>
2017-11-14 16:50:50 +0000

    refactor tools

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -24,14 +24,16 @@ class TopicsController < ApplicationController
   end
 
   def init_tools
+    @slug = params[:path].split('/').last
     @api_call = ToolQueries.new
-    @topic_tools = @api_call.topic_query
-    @tools_id = @topic_tools.first['id']
-    @taxonomy = 'it-and-technical-support'
+    # @topic_tools = @api_call.topic_query
+    # @tools_id = @topic_tools.first['id']
+    # @taxonomy = 'it-and-technical-support'
 
-    @tools_children_content = @api_call.tools_content_query(@tools_id, @taxonomy)
-    @tools_children_standard = @api_call.tools_standard_query(@tools_id, @taxonomy)
-    @tools = @api_call.tools_children(@tools_children_content, @tools_children_standard)
+    # @tools_children_content = @api_call.tools_content_query(@tools_id, @taxonomy)
+    # @tools_children_standard = @api_call.tools_standard_query(@tools_id, @taxonomy)
+    # @tools = @api_call.tools_children(@tools_children_content, @tools_children_standard)
+    @topic_tools = @api_call.topic_query
   end
 
   def init_related_news
--- app/models/tool_queries.rb
+++ app/models/tool_queries.rb
@@ -1,18 +1,18 @@
 class ToolQueries
-  def topic_query
-    WpApi.get_json_body('pages?slug=tools')
+  def tools_query
+    WpApi.get_json_body('pages?filter[topic_taxonomy]=data-hub')
   end
 
-  def tools_content_query(parent_id, taxonomy)
-    WpApi.get_json_body("pages?type=content&parent=#{parent_id}&filter[topic_taxonomy]=#{taxonomy}")
-  end
+  # def tools_content_query(parent_id, taxonomy)
+  #   WpApi.get_json_body("pages?type=content&parent=#{parent_id}&filter[topic_taxonomy]=#{taxonomy}")
+  # end
 
-  def tools_standard_query(parent_id, taxonomy)
-    WpApi.get_json_body("pages?type=standard_index&parent=#{parent_id}&filter[topic_taxonomy]=#{taxonomy}")
-  end
+  # def tools_standard_query(parent_id, taxonomy)
+  #   WpApi.get_json_body("pages?type=standard_index&parent=#{parent_id}&filter[topic_taxonomy]=#{taxonomy}")
+  # end
 
-  def tools_children(arr_a, arr_b)
-    array = arr_a + arr_b
-    array.sort_by { |hash| hash['title'].to_s }
-  end
+  # def tools_children(arr_a, arr_b)
+  #   array = arr_a + arr_b
+  #   array.sort_by { |hash| hash['title'].to_s }
+  # end
 end
