a6c99e23e235cba55ab4ac622f58f45a9741a18e
Peter Broom <me@peterbroom.co.uk>
2017-11-15 11:03:24 +0000

    add media query

--- app/assets/javascripts/equal-heights.js
+++ app/assets/javascripts/equal-heights.js
@@ -1,51 +1,53 @@
 (function() {
 
+	if (window.matchMedia("(min-width: 600px)").matches) {
 	//Gives Containers equal heights, using the tallest height and applying it to the other containers
 
-	//the function takes two arguments, the class of the container of all to-be-equalized containers, and the class of the containers themselves
-	function equalizeContainers(containerClass, targetClass) {
+		//the function takes two arguments, the class of the container of all to-be-equalized containers, and the class of the containers themselves
+		function equalizeContainers(containerClass, targetClass) {
 
-		//Get all containers of to-be-equalized elements...
-		var containerElements = document.getElementsByClassName(containerClass);
-
-		//... and iterate through them
-		for(var i = 0; i < containerElements.length; i++) {
-
-			// (Re)set the variable for the height of the biggest elements
-			var highestElement = 0;
-
-			// Get all to-be-equalized elements within the container
-			var targetElements = containerElements[i].getElementsByClassName(targetClass);
+			//Get all containers of to-be-equalized elements...
+			var containerElements = document.getElementsByClassName(containerClass);
 
 			//... and iterate through them
-			for(var j = 0; j < targetElements.length; j++) {
+			for(var i = 0; i < containerElements.length; i++) {
 
-				var currentTargetElement = targetElements[j];
+				// (Re)set the variable for the height of the biggest elements
+				var highestElement = 0;
 
-				// Set height of the elements auto, so the new "natural" height can be detected
-				currentTargetElement.style.height = "auto";
+				// Get all to-be-equalized elements within the container
+				var targetElements = containerElements[i].getElementsByClassName(targetClass);
 
-				// Get height of the element
-				var currentElementHeight = currentTargetElement.offsetHeight;
+				//... and iterate through them
+				for(var j = 0; j < targetElements.length; j++) {
 
-				//Compare height of the element to the highest height. If it's higher, this is the new highest height
-				if(currentElementHeight > highestElement) {
-					highestElement = currentElementHeight;
+					var currentTargetElement = targetElements[j];
+
+					// Set height of the elements auto, so the new "natural" height can be detected
+					currentTargetElement.style.height = "auto";
+
+					// Get height of the element
+					var currentElementHeight = currentTargetElement.offsetHeight;
+
+					//Compare height of the element to the highest height. If it's higher, this is the new highest height
+					if(currentElementHeight > highestElement) {
+						highestElement = currentElementHeight;
+					}
 				}
-			}
 
-			//Apply highest height to all to-be-equalized elements
-			for(var j = 0; j < targetElements.length; j++) {
-				targetElements[j].style.height = highestElement + "px";
-			}
+				//Apply highest height to all to-be-equalized elements
+				for(var j = 0; j < targetElements.length; j++) {
+					targetElements[j].style.height = highestElement + "px";
+				}
 
+			}
 		}
+
+		//Do everything on load and on browser-resize
+
+		window.onload = equalizeContainers('eh-container', 'eh-target');
+
+		window.onresize = equalizeContainers('eh-container', 'eh-target');
+
 	}
-
-	//Do everything on load and on browser-resize
-
-	window.onload = equalizeContainers('eh-container', 'eh-target');
-
-	window.onresize = equalizeContainers('eh-container', 'eh-target');
-
 }).call(this);
