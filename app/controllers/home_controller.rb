717d6a4f5fb188126afb34e88801e4b97684ed46
Christian Sutter <git@doublesignal.com>
2018-11-08 13:57:12 +0000

    Re-add deleted "How do I?" and "What's popular" blocks
    For some reason some random front-end PR about IE8 accordions completely
    deleted the backend code that populate the "How do I?" and "What's
    popular?" sections on the Digital Workspace homepage:
    https://github.com/uktrade/digital-workspace/pull/166
    
    This adds the code back in (only these two sections for now). We should
    evaluate why this was removed, and see whether we need to add some of
    the other deleted code back in as well.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
     @api_call = HomePageQueries.new
     @posts = @api_call.main_query
+    @howdois = @api_call.howdois
+    @popular_posts = @api_call.popular_posts
   end
 end
