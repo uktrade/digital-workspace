acd3bd75ba51f8bc8c31e4f385b82bc92ac8fa1f
Christian Sutter <git@doublesignal.com>
2018-12-06 15:17:59 +0000

    Add ability to disable SSL verification for WP connection
    This will help us develop against development WP instances.

--- app/models/wp_api.rb
+++ app/models/wp_api.rb
@@ -3,6 +3,9 @@ class WpApi
   BASE_CUSTOM_URI = ENV['WP_API_CUSTOM']
   AUTH_TOKEN = ENV['WP_API_KEY']
 
+  # Allow disabling SSL host verification in development environments
+  SSL_VERIFYHOST = ENV['SSL_VERIFYHOST'].present? ? 0 : 2
+
   class << self
     def get_json_body(base_path, params: {}, use_cache: true)
       path = base_path + '?' + params.to_query
@@ -41,7 +44,8 @@ class WpApi
         URI.join(BASE_URI, path).to_s,
         headers: {
           'Authorization' => "Basic #{AUTH_TOKEN}"
-        }
+        },
+        ssl_verifyhost: SSL_VERIFYHOST
       )
     end
 
@@ -50,7 +54,8 @@ class WpApi
         URI.join(BASE_CUSTOM_URI, path).to_s,
         headers: {
           'Authorization' => "Basic #{AUTH_TOKEN}"
-        }
+        },
+        ssl_verifyhost: SSL_VERIFYHOST
       )
     end
 
@@ -58,7 +63,8 @@ class WpApi
       Typhoeus.post(
         URI.join(BASE_URI, 'comments').to_s,
         body: json,
-        headers: { 'Authorization' => "Basic #{AUTH_TOKEN}" }
+        headers: { 'Authorization' => "Basic #{AUTH_TOKEN}" },
+        ssl_verifyhost: SSL_VERIFYHOST
       )
     end
 
@@ -81,7 +87,8 @@ class WpApi
         URI.join(BASE_CUSTOM_URI, path).to_s,
         headers: {
           'Authorization' => "Basic #{AUTH_TOKEN}"
-        }
+        },
+        ssl_verifyhost: SSL_VERIFYHOST
       )
     end
 
