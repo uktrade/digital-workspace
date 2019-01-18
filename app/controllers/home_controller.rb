ad8c9e397289bdc5e5320fd90abcef6864d6c092
Christian Sutter <git@doublesignal.com>
2019-01-18 13:49:31 +0000

    Fix notification banner not showing on homepage (#200)
    This is implemented in the most awful way imaginable, but this commit
    replicates the behaviour of the way it's done across the rest of the
    application for the sake of consistency.
    
    The notification banner is returned as an attribute on *every single
    content item type* in Wordpress. Every controller fetches the first
    item of content and chucks it into `@global_notification`, where the
    notification partial in the application layout picks up on it. The
    homepage wasn't doing that. Now it does.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,9 @@ class HomeController < ApplicationController
     @posts = @api_call.main_query
     @howdois = @api_call.howdois
     @popular_posts = @api_call.popular_posts
+
+    # TODO: This is awful but added like this to keep it consistent with other
+    #   controllers for now. Needs refactoring.
+    @global_notification = @posts.first if @posts.first.is_a?(Hash)
   end
 end
