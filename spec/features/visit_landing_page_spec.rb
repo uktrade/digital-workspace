83c19c2b18c0b604480b4cd5d2efee59c255a8b4
Christian Sutter <git@doublesignal.com>
2019-02-26 16:26:52 +0000

    Use "small" People Finder header on homepage (#208)
    - Remove "large" homepage People Finder header widget and CSS
    - Rename "small" `people_search_inner_page` partial to `people_search`
    - Slightly refactor People Finder profile display code (e.g. no fallback
      to `name` from `given_name` needed, as the latter is mandatory)

--- spec/features/visit_landing_page_spec.rb
+++ spec/features/visit_landing_page_spec.rb
@@ -11,11 +11,11 @@ describe 'visit the landing page', vcr: true do
     expect(page).to have_link('Digital Workspace')
     expect(page).to have_css('h2', text: 'What\'s popular?')
 
-    within('.profile-overview') do
-      expect(page).to have_link('Update', href: "#{ENV['PEOPLEFINDER_URL']}/my/profile")
+    within('.user-account') do
+      expect(page).to have_link('Please set up your profile', href: "#{ENV['PEOPLEFINDER_URL']}/my/profile")
     end
 
-    within('.welcome-people-finder') do
+    within('.people-search-inner-page') do
       form = find('form#people_search')
       expect(form['action']).to eq("#{ENV['PEOPLEFINDER_URL']}/search")
     end
