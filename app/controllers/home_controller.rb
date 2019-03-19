ae70793c3749c43d00e1a5a39999dfe698e9930b
Christian Sutter <git@doublesignal.com>
2019-03-19 14:49:58 +0000

    Add ability to show a "quick links" box on homepage (#214)
    This comes through from the Wordpress menus content type, and can have a
    title and multiple links. It won't be shown if the content doesn't come
    through (e.g. if the box is set to private)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,9 @@ class HomeController < ApplicationController
     @howdois = @api_call.howdois
     @popular_posts = @api_call.popular_posts
 
+    @quick_links_menu_title = @api_call.quick_links_menu_title
+    @quick_links_menu_content = @api_call.quick_links_menu_content
+
     # TODO: This is awful but added like this to keep it consistent with other
     #   controllers for now. Needs refactoring.
     @global_notification = @posts.first if @posts.first.is_a?(Hash)
