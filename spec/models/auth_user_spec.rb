3bccaaacb2ddda68d8bd0ec5309310069525ae53
Christian Sutter <git@doublesignal.com>
2019-04-04 17:20:39 +0100

    Use SSO user_id instead of email to fetch PF profile (#219)
    We currently fetch user profiles from People Finder based on a user's
    Omniauth email address - this should use the unique `user_id` instead.
    
    NOTE: Deploying this will sign out all currently signed in users.

--- spec/models/auth_user_spec.rb
+++ spec/models/auth_user_spec.rb
@@ -8,7 +8,7 @@ describe AuthUser do
       let(:omniauth_hash) { valid_omnniauth_user }
 
       it 'sets the email address' do
-        expect(subject.email).to eq('alice@example.com')
+        expect(subject.ditsso_user_id).to eq('deadbeef')
       end
     end
 
@@ -16,7 +16,7 @@ describe AuthUser do
       let(:omniauth_hash) { { name: 'bob' } }
 
       it 'fails gracefully' do
-        expect(subject.email).to be_nil
+        expect(subject.ditsso_user_id).to be_nil
       end
     end
   end
