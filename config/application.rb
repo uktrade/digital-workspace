17662950ec4253cbaeacddffce0a892b1864b825
Christian Sutter <git@doublesignal.com>
2020-02-13 11:47:11 +0000

    Use Rails cache store to store sessions (#272)
    This is currently using a third party gem and this brings it in line
    with People Finder. Also updates the configuration to match People
    Finder.

--- config/application.rb
+++ config/application.rb
@@ -23,16 +23,19 @@ Bundler.require(*Rails.groups)
 
 module DigitalWorkspace
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
     config.middleware.insert_after Rails::Rack::Logger, HealthCheck::MiddlewareHealthcheck
 
     config.gtm_id = ENV['GTM_ID']
     config.gtm_extra = ENV['GTM_EXTRA']
+    config.redis_cache_url = ENV['REDIS_URL']
+
+    # Caching and sessions
+    config.cache_store = :redis_cache_store, { url: config.redis_cache_url }
+    config.session_store :cache_store,
+                         key: 'workspace_session',
+                         expire_after: 1.hour,
+                         httponly: true
   end
 end
